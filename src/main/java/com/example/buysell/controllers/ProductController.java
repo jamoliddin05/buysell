e938faeacf81095b4e18216d4788cb08b40ec575
jamoliddin05 <jamol05@icloud.com>
2023-11-21 15:58:40 +0500

    Some commit

--- src/main/java/com/example/buysell/controllers/ProductController.java
+++ src/main/java/com/example/buysell/controllers/ProductController.java
@@ -1,12 +1,27 @@
 package com.example.buysell.controllers;
 
+import com.example.buysell.models.Product;
+import com.example.buysell.services.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
+
     @GetMapping("/")
-    public String products() {
+    public String products(Model model) {
+        model.addAttribute("products", productService.listProducts());
         return "products";
+    }
+
+    @PostMapping("/product/create")
+    public String createProduct(Product product) {
+        productService.saveProduct(product);
+        return "redirect:/";
     }
 }
