460dca3f8e757c81bafc81d661577d8dd1f25dc7
jamoliddin05 <jamol05@icloud.com>
2023-11-23 13:03:01 +0500

    Final commit here

--- src/main/java/com/example/buysell/models/User.java
+++ src/main/java/com/example/buysell/models/User.java
@@ -1,4 +1,51 @@
 package com.example.buysell.models;
 
+import com.example.buysell.models.enums.Role;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "emails", unique = true)
+    private String email;
+
+    @Column(name = "phoneNumbers")
+    private String phoneNumber;
+
+    @Column(name = "names")
+    private String name;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "image_id")
+    private Image avatar;
+
+    @Column(name = "password", length = 1000)
+    private String password;
+
+    @ElementCollection(targetClass = Role.class)
+    @CollectionTable(name = "user_role",
+    joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles = new HashSet<>();
+
+    private LocalDateTime dateOfCreation;
+
+    @PrePersist
+    private void init() {
+        dateOfCreation = LocalDateTime.now();
+    }
 }
