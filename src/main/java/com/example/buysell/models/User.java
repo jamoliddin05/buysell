b59428e99b372865bd27af5204db53b0d7e68e0a
jamoliddin05 <jamol05@icloud.com>
2023-11-23 14:49:04 +0500

    Checkpoint

--- src/main/java/com/example/buysell/models/User.java
+++ src/main/java/com/example/buysell/models/User.java
@@ -3,15 +3,18 @@ package com.example.buysell.models;
 import com.example.buysell.models.enums.Role;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(name = "users")
 @Data
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
@@ -47,5 +50,35 @@ public class User {
     @PrePersist
     private void init() {
         dateOfCreation = LocalDateTime.now();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return active;
     }
 }
