fbaa74d4468c9a7e97496ad749f8083bb5d8fef1
jamoliddin05 <jamol05@icloud.com>
2023-11-24 15:37:06 +0500

    Everything now works

--- src/main/java/com/example/buysell/services/UserService.java
+++ src/main/java/com/example/buysell/services/UserService.java
@@ -8,6 +8,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -24,5 +30,40 @@ public class UserService {
         log.info("Saving new User with email: {}", email);
         userRepository.save(user);
         return true;
+    }
+
+    public void deleteUserById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public List<User> list() {
+        return userRepository.findAll();
+    }
+
+    public void banUser(Long id) {
+        User user = userRepository.findById(id).orElse(null);
+        if(user != null) {
+            if(user.isActive()) {
+                user.setActive(false);
+                log.info("Banning user with id = {}; email = {}", id, user.getEmail());
+            } else {
+                user.setActive(true);
+                log.info("Unbanning user with id = {}; email = {}", id, user.getEmail());
+            }
+        }
+        userRepository.save(user);
+    }
+
+    public void changeUserRoles(User user, Map<String, String> form) {
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+        user.getRoles().clear();
+        for(String key : form.keySet()) {
+            if(roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepository.save(user);
     }
 }
